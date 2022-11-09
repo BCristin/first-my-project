33342c5acd16b8db9a4b20c82a3aca88878d8919
Cristin Balan <tianncristiann@gmail.com>
2022-11-09 19:24:02 +0200

    add modal

--- js/main.js
+++ js/main.js
@@ -1,21 +1,35 @@
 // menu si butonul din meniu
 window.addEventListener('DOMContentLoaded', () => {
-  const menu = document.querySelector('.nav-list'),
-  menuItem = document.querySelectorAll('.nav-item'),  
-  hamburger = document.querySelector('.hamburger');
-  body = document.querySelector('.body');
+    const menu = document.querySelector('.nav-list'),
+        menuItem = document.querySelectorAll('.nav-item'),
+        hamburger = document.querySelector('.hamburger');
+    //   body = document.querySelector('.body');
 
-  hamburger.addEventListener('click', () => {
-      hamburger.classList.toggle('hamburger_active');
-      menu.classList.toggle('menu_active');
-      body.classList.toggle('no__scroll');
-  });
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+        //   body.classList.toggle('no__scroll');
+    });
 
-  menuItem.forEach(item => {
-      item.addEventListener('click', () => {
-          hamburger.classList.toggle('hamburger_active');
-          menu.classList.toggle('menu_active');
-          body.classList.toggle('no__scroll');
-      })
-  })
-})
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+            //   body.classList.toggle('no__scroll');
+        })
+    })
+});
+
+
+
+(function ($) {
+    $(function () {
+        //Modal
+        $('[data-modal=contact-me]').on('click', function () {
+            $('.overlay, #contact-me').fadeIn('slow');
+        });
+        $('.modal__close').on('click', function () {
+            $('.overlay, #contact-me').fadeOut('slow');
+        });
+    });
+})(jQuery);
